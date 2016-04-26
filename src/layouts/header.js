2d8a22aee6235b849cb13cac8bd381240098348b
subramaniashiva <vengaishiva@gmail.com>
2016-04-26 15:37:36 +0200

    Added header menu and styles

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -9,11 +9,27 @@ export class Header extends Component {
   render() {
     return (
       <header className = 'header clearfix'>
-        <div className = 'pull-left'>
-          <a href='/' className = 'logo'>Donando</a>
-        </div>
-        <div className = 'pull-right'>
-        </div>
+        <nav className='navbar navbar-default'>
+          <div className='container-fluid'>
+            <div className='navbar-header'>
+              <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#main-menu' aria-expanded='false'>
+                <span className='sr-only'>Toggle navigation</span>
+                <span className='icon-bar'></span>
+                <span className='icon-bar'></span>
+                <span className='icon-bar'></span>
+              </button>
+              <a className='navbar-brand' href='#'>Donando</a>
+            </div>
+            <div className='collapse navbar-collapse clearfix' id='main-menu'>
+              <div className='menu-list'>
+                <ul className='nav navbar-nav'>
+                  <li className='active'><a href='#'>Link <span className='sr-only'>(current)</span></a></li>
+                  <li><a href='#'>Link</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
         {/*
           {this.props.text}
           <button onClick = {() => (this.props.dispatch(action_1({text: 'This is from UI'})))}>Click to dispatch action</button>
