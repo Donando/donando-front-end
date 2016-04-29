666e99397a487e8c1051d69c9322f7368e39a743
subramaniashiva <vengaishiva@gmail.com>
2016-04-29 12:34:22 +0200

    Updated main layout to have props

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import Header from './header'
-import TESTMap from './../components/TESTMap'
 
 export class Main extends Component {
   render() {
     return (
       <div className = 'container'>
         <Header />
-        {this.props.children}
-        <TESTMap/>
+        <div className = 'relative'>
+          <div className={this.props.searchStatus === 'loading'? 'loading-container': 'loading-container hide'}>
+            <p>Loading results....<br/>Please wait</p>
+          </div>
+          {this.props.children}
+        </div>
       </div>)
   }
 }
 
+Main.propTypes = {
+  searchStatus: React.PropTypes.string.isRequired
+}
 function mapStateToProps(state) {
   return state
 }
