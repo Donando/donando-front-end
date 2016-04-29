60c65aad463dcf022ec7e1c28ed55aaf7db358d0
subramaniashiva <vengaishiva@gmail.com>
2016-04-29 12:35:28 +0200

    Updated search results page with map

--- src/containers/search-results.js
+++ src/containers/search-results.js
@@ -1,38 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { change_filter_location } from 'redux/reducers/filter-reducer'
+import { load_location_data } from 'redux/reducers/filter-reducer'
+
+import Map from 'components/map'
 
 import 'styles/search-results.scss'
 
 export class SearchResults extends Component {
   componentWillMount() {
-    this.props.dispatch(change_filter_location(this.props.params.name));
+    this.props.dispatch(load_location_data(this.props.params.name));
   }
   render() {
+    let markerObjects = [];
+    let searchResult = this.props.demands && this.props.demands.map((item, index) => {
+      markerObjects.push(item.ngo);
+      return (
+        <div className='search-result' key={index}>
+          <p>{item.data}</p>
+          <p>{item.ngo.name}</p>
+          <p>{item.ngo.phone}</p>
+          <p>{item.ngo.email}</p>
+          <p>{item.ngo.address}</p>
+          <p>{item.ngo.latitude}</p>
+          <p>{item.ngo.longitude}</p>
+        </div>
+      )
+    });
+
     return (
       <div>
         <p>You search query: {this.props.params.name}</p>
+        <Map markers = {markerObjects} />
         <p>Current value of API call : {this.props.searchStatus}</p>
         {
-          this.props.demands && this.props.demands.map((item, index) => {
-            return (
-              <div className='search-result' key={index}>
-                <p>{item.data}</p>
-                <p>{item.ngo.name}</p>
-                <p>{item.ngo.phone}</p>
-                <p>{item.ngo.email}</p>
-                <p>{item.ngo.address}</p>
-              </div>
-            )
-          })
+          searchResult
         }
       </div>
     )
   }
 }
 SearchResults.propTypes = {
-  demands: React.PropTypes.Array,
+  demands: React.PropTypes.array,
   searchStatus: React.PropTypes.string
 }
 function mapStateToProps(state) {
