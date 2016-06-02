81e456be13befdf8164fcb7eb05ac73dd48d5297
Princiya Marina Sequeira <princiya777@yahoo.co.in>
2016-06-02 22:53:17 +0200

    server.js port changed

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var path  = require('path')
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('App listening on port '+app.get('port'));
 });
