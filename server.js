d9ceefe11ba2db748f9591d988b28ca9c6bcb7ee
Princiya Marina Sequeira <princiya777@yahoo.co.in>
2016-06-02 23:19:52 +0200

    dir changed

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var path  = require('path')
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
 app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+});
 
 app.listen(app.get('port'), function () {
   console.log('App listening on port '+app.get('port'));
