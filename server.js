90f28aadb6afc7d8f764855a07a8fb0b1c95bf96
Princiya Marina Sequeira <princiya777@yahoo.co.in>
2016-06-02 23:46:09 +0200

    gitignore file changed to add dist dir to heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + './dist/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(app.get('port'), function () {
