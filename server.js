98fd2e23cf83fd911c7f49791702886d3027ff5d
Princiya Marina Sequeira <princiya777@yahoo.co.in>
2016-06-02 23:12:22 +0200

    reverted

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path  = require('path')
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'dist')));
